258d56122b8a40b094ff56f5c8722e784885e5fb
Henrik Picton <blaberstien@gmail.com>
2025-10-10 13:06:55 +0200

    R script

--- R_script/r_script.R
+++ R_script/r_script.R
@@ -1,13 +1,60 @@
 library(tidyverse)
+library(gt)
+library(irr)
+
 
 descriptives <- read_csv("Wingate_data.csv")
 
 modified_descriptives <- descriptives %>% 
   select(FP, Test, Wsnitt) %>% 
-  pivot_wider(
-    names_from = Test,
-    values_from = Wsnitt,
-    names_prefix = "Test"
-  )
+  group_by(FP) %>% 
+  summarise(Gjennomsnitt = mean(Wsnitt),
+            SD = sd(Wsnitt))
 
 modified_descriptives
+
+descriptive_table <- modified_descriptives %>% 
+  gt() %>% 
+  tab_header(
+    title = "Gjennomsnittlig Watt fra økter",
+    subtitle = "Presentert med gjennomsnitt og standard avvik (SD)"
+  ) %>% 
+  fmt_number(
+    columns = everything(),
+    decimals = 0
+  ) %>% 
+  tab_style(
+    style = cell_text(weight = "bold"),
+    locations = cells_column_labels(everything())
+  )
+
+descriptive_table
+
+
+descriptives_wide <- descriptives %>% 
+  select(FP,Test, Wsnitt) %>% 
+  pivot_wider(names_from = Test, values_from = Wsnitt)
+
+icc_descriptives <- descriptives_wide %>% select(-FP)
+
+head(icc_descriptives)
+
+icc_result <- icc(
+  icc_descriptives,
+  model = "twoway",
+  type = "consistency",
+  unit = "average"
+)
+
+print(icc_result)
+
+
+ggplot(descriptives, aes(x = factor(Test), y = Wsnitt, group = FP)) +
+  geom_line(alpha = 0.5) +
+  geom_point() +
+  theme_minimal() +
+  labs(
+    x = "Test",
+    y = "Gjennomsnittlig W",
+    title = "Repeterte gjennomsnittlige W-målinger per deltaker"
+  )
