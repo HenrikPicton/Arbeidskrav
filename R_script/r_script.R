1b63c207acdf41f4ecd4b4fa7fcccfb4186c6304
Henrik Picton <blaberstien@gmail.com>
2025-10-10 08:34:07 +0200

    endringer

--- R_script/r_script.R
+++ R_script/r_script.R
@@ -2,4 +2,12 @@ library(tidyverse)
 
 descriptives <- read_csv("Wingate_data.csv")
 
+modified_descriptives <- descriptives %>% 
+  select(FP, Test, Wsnitt) %>% 
+  pivot_wider(
+    names_from = Test,
+    values_from = Wsnitt,
+    names_prefix = "Test"
+  )
 
+modified_descriptives
